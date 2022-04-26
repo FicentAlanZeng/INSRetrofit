547908157201412becc13ab58ca92ccb496dd3af
Alan <876224031@qq.com>
2022-04-26 14:55:33 +0800

    add core

---
+++ Demo/Test_F/Test_F/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test_F
+//
+//  Created by Alan on 2022/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Source/INSRetrofit_Framework.framework/Headers/INSRetrofit_Framework.h
@@ -0,0 +1,18 @@
+//
+//  INSRetrofit_Framework.h
+//  INSRetrofit_Framework
+//
+//  Created by Alan on 2022/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for INSRetrofit_Framework.
+FOUNDATION_EXPORT double INSRetrofit_FrameworkVersionNumber;
+
+//! Project version string for INSRetrofit_Framework.
+FOUNDATION_EXPORT const unsigned char INSRetrofit_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <INSRetrofit_Framework/PublicHeader.h>
+
+
